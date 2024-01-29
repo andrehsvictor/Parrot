c77fe288ab75023ce70d47b97e87a7bc5b470037
André Victor <andrehsvictor@gmail.com>
2024-01-29 15:49:00 -0300

    Refactor App component to use HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Code } from "@mui/icons-material";
-import { Container, Typography } from "@mui/material";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <Typography variant="h3" component="h1" align="center">
-        Under development
-      </Typography>
+      <HomePage />
     </>
   );
 }
