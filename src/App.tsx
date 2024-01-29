a00a8b2826e876e9fc3e64e7a51d3b947cbbdaad
André Victor <andrehsvictor@gmail.com>
2024-01-29 20:44:29 -0300

    Add NavBar component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
+      <NavBar />
       <HomePage />
     </>
   );
