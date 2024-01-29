0a8e575066f7f3baabfe117ec52d91c3c9c6a47a
André Victor <andrehsvictor@gmail.com>
2024-01-29 11:07:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Code } from "@mui/icons-material";
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h3" component="h1" align="center">
+        Under development
+      </Typography>
+    </>
+  );
+}
+
+export default App;
