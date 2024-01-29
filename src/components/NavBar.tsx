5c60c086e8127c699d123943344592958bd856bb
André Victor <andrehsvictor@gmail.com>
2024-01-29 20:44:34 -0300

    Add NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,34 @@
+import { AccountCircle, Https } from "@mui/icons-material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+
+export default function NavBar() {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: "green",
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              Parrot™
+            </Typography>
+            <Typography
+              variant="caption"
+              component="div"
+              sx={{ flexGrow: 1, ml: 1 }}
+            >
+              A social media platform for parrots
+            </Typography>
+            <Button color="inherit" variant="text" size="medium">
+              <AccountCircle sx={{ mr: 1 }} />
+              Sign in
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+}
