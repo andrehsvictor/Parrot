15ca545bb7d683910c68410ef4f194984faedab4
André Victor <andrehsvictor@gmail.com>
2024-01-30 10:27:28 -0300

    Refactor HomePage component to display a list of posts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,32 @@
-import { Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Post } from "../types/Post";
+import { API_URL } from "../utils/Utils";
+import PostCard from "../components/PostCard";
 
 export default function HomePage() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    fetch(`${API_URL}/api/v1.0/posts`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  });
   return (
     <>
-      <Typography variant="h2" component="div">
-        Home page, coming soon! 🚧
-      </Typography>
+      <Container
+        maxWidth="sm"
+        sx={{
+          maxHeight: "80vh",
+          overflow: "auto",
+        }}
+      >
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </Container>
     </>
   );
 }
